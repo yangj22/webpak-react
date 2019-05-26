385b689fc4e4c4e2b3f36abe1d550bd65c8d1766
yangjie19920911@sina.com <wow651545984>
2019-05-26 21:27:37 +0800

    添加 css 和 less 配置

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React,{Component} from "react"
 
 import ReactDom from 'react-dom'
 import Home from './page/home'
-
+import '../public/asset/index.less'
 class App extends Component{
     render(){
         return <div>
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,15 @@ module.exports={
               test:/\.(js|jsx)$/,
               use:['babel-loader'],
               exclude:/node_modules/
+          },
+          {
+              test:/\.css$/,
+              use:['style-loader','css-loader']
+          },
+          {
+              test:/\.less$/,
+              use:['style-loader','css-loader','less-loader'],
+
           }
       ]
     },
